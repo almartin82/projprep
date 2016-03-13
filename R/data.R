61e2dfc1188251817670946111856fdf8b044e8b
almartin82 <almartin@gmail.com>
2016-03-13 13:05:30 -0400

    cmd check stuff

---
+++ R/data.R
@@ -0,0 +1,46 @@
+#' id_map
+#'
+#' dataframe that maps names/ids across various mlb data sources, by
+#' Tanner: http://www.smartfantasybaseball.com/tools/
+#'
+#' @format
+#' \describe{
+#' \item{idplayer}{idplayer}
+#' \item{playername}{playername}
+#' \item{birthdate}{birthdate}
+#' \item{firstname}{firstname}
+#' \item{lastname}{lastname}
+#' \item{team}{team}
+#' \item{lg}{lg}
+#' \item{pos}{pos}
+#' \item{idfangraphs}{idfangraphs}
+#' \item{fangraphsname}{fangraphsname}
+#' \item{mlbid}{mlbid}
+#' \item{mlbname}{mlbname}
+#' \item{cbsid}{cbsid}
+#' \item{cbsname}{cbsname}
+#' \item{retroid}{retroid}
+#' \item{brefid}{brefid}
+#' \item{nfbcid}{nfbcid}
+#' \item{nfbcname}{nfbcname}
+#' \item{espnid}{espnid}
+#' \item{espnname}{espnname}
+#' \item{kfflname}{kfflname}
+#' \item{davenportid}{davenportid}
+#' \item{bpid}{bpid}
+#' \item{yahooid}{yahooid}
+#' \item{yahooname}{yahooname}
+#' \item{mstrbllname}{mstrbllname}
+#' \item{bats}{bats}
+#' \item{throws}{throws}
+#' \item{fantprosname}{fantprosname}
+#' \item{lastcommafirst}{lastcommafirst}
+#' \item{rotowireid}{rotowireid}
+#' \item{fanduelname}{fanduelname}
+#' \item{fanduelid}{fanduelid}
+#' \item{draftkingsname}{draftkingsname}
+#' \item{ottoneuid}{ottoneuid}
+#' \item{hqid}{hqid}
+#' }
+#' @source smartfantasybaseball.com
+"id_map"
