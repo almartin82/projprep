b9c10de4de7213a4fc29bd255fa9746261679c47
almartin82 <almartin@gmail.com>
2016-03-14 21:51:18 -0400

    refactor fangraphs scrape to preserve position eligibility

--- R/fangraphs.R
+++ R/fangraphs.R
@@ -11,22 +11,27 @@
 
 scrape_fangraphs <- function(bat_pitch, proj_system) {
 
-  base_fangraphs <- 'http://www.fangraphs.com/projections.aspx?'
-  url_params <- sprintf('pos=all&stats=%s&type=%s&team=', bat_pitch, proj_system)
+  base_fangraphs <- 'http://www.fangraphs.com/projections.aspx?pos='
   if (bat_pitch == 'bat') {
     end_params <- '&players=0&sort=26,d'
+    pos_choices <- c('c', '1b', '2b', 'ss', '3b', 'of')
   } else if (bat_pitch == 'pit') {
     end_params <- '&players=0&sort=9,d'
+    pos_choices <- c('all')
   }
-  urls <- paste0(
-    base_fangraphs, url_params, c(1:30), end_params
-  )
+
+  url1 <- paste0(base_fangraphs, pos_choices)
+  url_params <- sprintf('&stats=%s&type=%s&team=', bat_pitch, proj_system)
+  url2 <- paste0(url1, url_params)
+  url3 <- expand.grid(url2, c(1:30))
+  urls <- paste0(url3$Var1, url3$Var2, end_params)
 
   proj_list <- lapply(urls, function(x){
       team_table <- XML::readHTMLTable(
         x, as.data.frame = TRUE, stringsAsFactors = FALSE
       )
       df <- team_table$ProjectionBoard1_dg1_ctl00
+      df$url <- x
 
       df
     }
