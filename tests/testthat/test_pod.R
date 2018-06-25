fc996565fe26b8eb93bc3a9f9026116c1bd6f1db
almartin82 <almartin@nps.k12.nj.us>
2018-06-25 17:30:14 -0400

    updates

--- tests/testthat/test_pod.R
+++ tests/testthat/test_pod.R
@@ -1,5 +1,13 @@
 context('pod')
 
+pod_file <- file.path('paid_projections', 'pod_projections.xlsx')
+pod_file_loc <- file.path(getwd(), pod_file)
+#for CMD check
+pod_file_loc <- gsub('.Rcheck', '', pod_file_loc, fixed = TRUE)
+#for devtools::test()
+pod_file_loc <- gsub('tests/testthat/', '', pod_file_loc, fixed = TRUE)
+pod_ex <- read_raw_pod(pod_file_loc)
+
 
 test_that('read_raw_pod correctly reads pod excel file', {
   expect_is(pod_ex, 'list')
